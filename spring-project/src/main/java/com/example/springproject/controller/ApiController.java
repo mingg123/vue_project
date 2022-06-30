ec9c24b554a64848e01ac87c24e8aa4aa6dbe623
mingg123 <spgb123spgb@gmail.com>
2022-06-30 22:18:17 +0900

    feat : add delete controller

--- spring-project/src/main/java/com/example/springproject/controller/ApiController.java
+++ spring-project/src/main/java/com/example/springproject/controller/ApiController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Slf4j
@@ -29,17 +30,16 @@ public class ApiController {
   @PostMapping("/addItem")
   public List<ItemDto> addItem(@RequestBody ItemDto itemDto) {
     log.info("추가 : {}", itemDto);
-    System.out.println(itemDto);
     List<ItemDto> items = new ArrayList<>();
     items.add(itemDto);
     return items;
   }
 
-  @DeleteMapping("/{index}")
-  public List<ItemDto> deleteItem(@PathVariable int index) {
-    log.info("delete index : {}", index);
-    List<ItemDto> items = new ArrayList<>();
-    return items;
+  @DeleteMapping("/{id}")
+  public List<Item> deleteItem(@PathVariable String id) {
+    log.info("delete id : {}", id);
+    itemRepository.deleteById(id);
+    return itemRepository.findAll();
   }
 
 
