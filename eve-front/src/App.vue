e54c582089f4ccdaee99929cff4015c607e88c9a
minjikim7983 <minjikim7983@nexon.co.kr>
2022-07-04 10:09:40 +0900

    reafctor : 불필요 파일 제거

--- eve-front/src/App.vue
+++ eve-front/src/App.vue
@@ -1,26 +1,42 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <img
+        src="./assets/image/common/calendar.png"
+        class="popup_btn"
+        v-on:click="onClickPopup"
+      />
+      <Frame v-if="showFramePopup" />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { computed, defineComponent } from "vue";
+import Frame from "@/components/container/Frame.vue";
+import { useStore } from "vuex";
+import store from "@/store";
 
 export default defineComponent({
   name: "App",
 
-  components: {
-    HelloWorld,
-  },
-
-  data() {
+  components: { Frame },
+  setup() {
+    const store = useStore();
     return {
-      //
+      showFramePopup: computed(() => store.getters.getShowFramePopup),
     };
+  },
+  methods: {
+    onClickPopup: function () {
+      store.dispatch("setShowFramePopup", true);
+    },
   },
 });
 </script>
+<style lang="scss">
+.popup_btn {
+  height: 50px;
+  width: 50px;
+}
+</style>
